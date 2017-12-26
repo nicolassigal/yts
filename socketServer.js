95930dd3f982c03a0fa02baae3c0c8d40c4d2b6e
nicolassigal <nicolas.sigal@fisglobal.com>
2017-12-26 10:23:10 -0300

    asd

--- socketServer.js
+++ socketServer.js
@@ -1,19 +1,15 @@
 'use strict';
+const search = require('youtube-search');
+const express = require('express');
+const socketIO = require('socket.io');
+const path = require('path');
+const opts = { maxResults: 50, key: 'AIzaSyCnqAFM5z0dsC_gPE-DQeFrQe2PScejMMw' };
+const PORT = process.env.PORT || 3000;
+const INDEX = path.join(__dirname, 'index.html');
 
-const express = require('express'),
-      socketIO = require('socket.io'),
-      path = require('path'),
-      search = require('youtube-search'),
-      ffmpeg = require('@ffmpeg-installer/ffmpeg'),
-      YoutubeMp3Downloader = require("youtube-mp3-downloader"),
-      PORT = process.env.PORT || 3000,
-      INDEX = path.join(__dirname, 'index.html'),
-      server = express(),
-      opts = { maxResults: 50, key: 'AIzaSyCnqAFM5z0dsC_gPE-DQeFrQe2PScejMMw' };
-
-server
-    .use((req, res) => res.sendFile(INDEX) )
-    .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+const server = express()
+  .use((req, res) => res.sendFile(INDEX) )
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 const io = socketIO(server);
 
@@ -22,7 +18,7 @@ io.on('connection', (socket) => {
   socket.on('search', query => {
     search(query, opts, function(err, results) {
         if(err) return console.log(err);
-        socket.emit('search', {results: results});
+        socket.emit('search', {results: results})
     });
   })
   socket.on('disconnect', () => console.log('Client disconnected'));
